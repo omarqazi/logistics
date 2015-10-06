cf376329fc57d2dec75e7b5f0ddb6855ea04fd82
Omar Qazi <omar@bqe.com>
2015-10-05 19:02:31 -0700

    added support for signatures

--- auth/auth.go
+++ auth/auth.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
+	"io"
 )
 
 func GeneratePrivateKey(bits int) (p *rsa.PrivateKey, e error) {
@@ -21,4 +24,56 @@ func StringForPrivateKey(p *rsa.PrivateKey) string {
 	bytes := x509.MarshalPKCS1PrivateKey(p)
 	bsf := base64.URLEncoding.EncodeToString(bytes)
 	return bsf
+}
+
+func PrivateKeyFromString(pem string) (*rsa.PrivateKey, error) {
+	der, err := base64.URLEncoding.DecodeString(pem)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := x509.ParsePKCS1PrivateKey(der)
+	return key, err
+}
+
+func StringForPublicKey(p *rsa.PublicKey) (string, error) {
+	bytes, err := x509.MarshalPKIXPublicKey(p)
+	if err != nil {
+		return "", err
+	}
+
+	der := base64.URLEncoding.EncodeToString(bytes)
+	return der, nil
+}
+
+func PublicKeyFromString(pk string) (*rsa.PublicKey, error) {
+	der, err := base64.URLEncoding.DecodeString(pk)
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := x509.ParsePKIXPublicKey(der)
+	rsaKey, ok := key.(*rsa.PublicKey)
+	if ok {
+		return rsaKey, err
+	}
+	return nil, err
+}
+
+func SignMessageWithKey(key *rsa.PrivateKey, msg string) (sig []byte, err error) {
+	hashFunction := sha256.New()
+	io.WriteString(hashFunction, msg)
+	hashSum := hashFunction.Sum(nil)
+
+	sig, err = rsa.SignPSS(rand.Reader, key, crypto.SHA256, hashSum, nil)
+	return
+}
+
+func ValidateSignatureForMessage(msg string, sig []byte, pub *rsa.PublicKey) (err error) {
+	hashFunction := sha256.New()
+	io.WriteString(hashFunction, msg)
+	hashSum := hashFunction.Sum(nil)
+
+	err = rsa.VerifyPSS(pub, crypto.SHA256, hashSum, sig, nil)
+	return
 }
--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -24,5 +25,74 @@ func TestGenerateKey(t *testing.T) {
 
 	if len(shittyKeyString) >= len(betterKeyString) {
 		t.Error("Error: bits property of GeneratePrivateKey seems to be ignored")
+	}
+}
+
+func TestParsePrivateKey(t *testing.T) {
+	key, err := GeneratePrivateKey(1024)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keyString := StringForPrivateKey(key)
+	parsedKey, err := PrivateKeyFromString(keyString)
+	if err != nil {
+		t.Fatal("Error parsing key", err)
+	}
+
+	if err := parsedKey.Validate(); err != nil {
+		t.Fatal("Error validating parsed key:", err)
+	}
+
+	if StringForPrivateKey(parsedKey) != keyString {
+		t.Error("Error: Parsed private key does not match original")
+	}
+}
+
+func TestParsePublicKey(t *testing.T) {
+	privateKey, err := GeneratePrivateKey(1024)
+	if err != nil {
+		t.Fatal("Error generating private key", err)
+	}
+
+	publicKey := privateKey.PublicKey
+	publicKeyString, err := StringForPublicKey(&publicKey)
+	if err != nil {
+		t.Fatal("Error marshaling public key:", err)
+	}
+
+	parsedKey, err := PublicKeyFromString(publicKeyString)
+	if err != nil {
+		t.Fatal("Error parsing marshaled public key:", err)
+	}
+
+	pks, err := StringForPublicKey(parsedKey)
+	if err != nil {
+		t.Fatal("Error marshaling public key", err)
+	}
+
+	if pks != publicKeyString {
+		t.Error("Error: parsed public key does not match")
+	}
+
+	fmt.Println(publicKeyString)
+}
+
+func TestSign(t *testing.T) {
+	privateKey, err := GeneratePrivateKey(1024)
+	if err != nil {
+		t.Fatal("Error generating private key:", err)
+	}
+
+	sig, err := SignMessageWithKey(privateKey, "hello world")
+	if err != nil {
+		t.Fatal("Error signing message", err)
+	}
+
+	fmt.Println(sig)
+
+	publicKey := privateKey.PublicKey
+	if err := ValidateSignatureForMessage("hello world", sig, &publicKey); err != nil {
+		t.Fatal("Error verifying signature:", err)
 	}
 }
