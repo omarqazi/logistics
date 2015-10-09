6d6b6786dea802ff001221dff3290229de6b5b99
Omar Qazi <omar@bqe.com>
2015-10-09 13:44:05 -0700

    implemented websocket connecticons for receiving notification updates

--- locations.go
+++ locations.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"fmt"
-	"logistics/datastore"
+	_ "github.com/omarqazi/logistics/auth"
+	"github.com/omarqazi/logistics/datastore"
+	"golang.org/x/net/websocket"
+	"log"
 	"net/http"
 )
+
+var webSocketHandler = websocket.Handler(WebSocketServer)
 
 type LocationsController struct {
 }
 
 func (l LocationsController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is the locations controller", datastore.Redis)
+	user, err := datastore.GetUser(r.URL.Path)
+	if err != nil {
+		log.Println("Error getting user:", err)
+		http.Error(w, "Error getting user", 500)
+		return
+	}
+	if user == nil {
+		http.Error(w, "User does not exist", 404)
+		return
+	}
+
+	//rsaKey, err := user.RSAKey()
+	//if err != nil {
+	//	http.Error(w,"Error authenticating",500)
+	//	return
+	//}
+
+	//if ok := auth.Request(w,r,rsaKey); !ok {
+	//	return
+	//}
+
+	webSocketHandler.ServeHTTP(w, r)
+}
+
+func WebSocketServer(ws *websocket.Conn) {
+	r := ws.Request()
+	user, err := datastore.GetUser(r.URL.Path)
+	if err != nil || user == nil {
+		return
+	}
+
+	for {
+		_, payload, err := datastore.ReceiveMessage(user.Channel())
+		if err != nil {
+			fmt.Fprintln(ws, "Error:", err)
+			return
+		}
+		fmt.Fprintln(ws, payload)
+	}
 }
