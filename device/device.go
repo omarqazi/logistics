79de354138855bbea4cea1c235b7e9cb1f0c861a
Omar Qazi <omar@bqe.com>
2016-02-09 19:38:46 -0800

    Pass correct arguments to SNS initializer

--- device/device.go
+++ device/device.go
@@ -1,40 +1,42 @@
 package device
 
 import (
+	"encoding/json"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
 	"log"
-	"encoding/json"
 )
 
 var notificationClient *sns.SNS
+
 func init() {
 	config := aws.NewConfig().WithRegion("us-west-2")
-	notificationClient = sns.New(config)
+	notificationClient = sns.New(session.New(), config)
 }
 
-func Push(payload map[string]interface{},deviceArn string) error {
+func Push(payload map[string]interface{}, deviceArn string) error {
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		return err
 	}
 	notificationPayload := map[string]string{
-		"APNS_SANDBOX" : string(payloadBytes),
+		"APNS_SANDBOX": string(payloadBytes),
 	}
 	finalBytes, err := json.Marshal(notificationPayload)
 	if err != nil {
 		return err
 	}
 	params := &sns.PublishInput{
-		Message: aws.String(string(finalBytes)),
+		Message:          aws.String(string(finalBytes)),
 		MessageStructure: aws.String("json"),
-		TargetArn: aws.String(deviceArn),
+		TargetArn:        aws.String(deviceArn),
 	}
 	_, err = notificationClient.Publish(params)
 	if err != nil {
-		log.Println("Error sending notification:",err)
+		log.Println("Error sending notification:", err)
 		return err
 	}
-	
+
 	return nil
 }
